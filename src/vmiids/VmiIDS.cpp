eaa054af648eb6332c9b474ae8a5257a0bcb7475
Thomas Kittel <kittel@in.tum.de>
2010-06-26 21:53:37 +0200

    Added base for VmiIDS

---
+++ src/vmiids/VmiIDS.cpp
@@ -0,0 +1,17 @@
+/*
+ * VmiIDS.cpp
+ *
+ *  Created on: Jun 26, 2010
+ *      Author: kittel
+ */
+
+#include "VmiIDS.h"
+
+VmiIDS::VmiIDS() {
+	// TODO Auto-generated constructor stub
+
+}
+
+VmiIDS::~VmiIDS() {
+	// TODO Auto-generated destructor stub
+}
---
+++ src/vmiids/VmiIDS.h
@@ -0,0 +1,17 @@
+/*
+ * VmiIDS.h
+ *
+ *  Created on: Jun 26, 2010
+ *      Author: kittel
+ */
+
+#ifndef VMIIDS_H_
+#define VMIIDS_H_
+
+class VmiIDS {
+public:
+	VmiIDS();
+	virtual ~VmiIDS();
+};
+
+#endif /* VMIIDS_H_ */
