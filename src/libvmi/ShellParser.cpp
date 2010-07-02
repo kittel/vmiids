d1af73ed5c1830ba653078b375889d72759d9df7
Thomas Kittel <kittel@in.tum.de>
2010-07-02 14:16:24 +0200

    Fixed the loginfunciotn in ShellParser

--- src/libvmi/ShellParser.cpp
+++ src/libvmi/ShellParser.cpp
@@ -17,47 +17,48 @@
 
 namespace libVMI {
 
-ShellParser::ShellParser() : ConsoleMonitor(SHELLDEVICE, LOGINSHELL){
+ShellParser::ShellParser() :
+	ConsoleMonitor(SHELLDEVICE, LOGINSHELL) {
 
+	LIBVMI_DEBUG_MSG("Constructor called\n");
 	this->loggedin = false;
 	try {
 		this->loggedin = this->isLoggedin();
-				
-		if(!this->loggedin){
+
+		if (!this->loggedin) {
 			LIBVMI_DEBUG_MSG("Trying to login\n");
 			this->loggedin = this->login("rootkitvm", "rootkitvm");
-		}else{
+		} else {
 			LIBVMI_DEBUG_MSG("Already logged in\n");
 			this->monitorShell = COMMANDSHELL;
 		}
-	}
-	catch(const char * exception){
+	} catch (const char * exception) {
 		LIBVMI_DEBUG_MSG("%s\n", exception);
 	}
 }
 
-ShellParser::~ShellParser(){
+ShellParser::~ShellParser() {
 }
 
-bool ShellParser::isLoggedin(void){
+bool ShellParser::isLoggedin(void) {
 	std::string searchString;
 	std::string replyString;
 	std::string lastLine;
 
 	this->sendCommand("");
-	for(int searchCount = 1000 ; searchCount > 0; searchCount--){
+	for (int searchCount = 1000; searchCount > 0; searchCount--) {
 		usleep(1000);
 		parseOutput(replyString);
 		searchString.append(replyString);
-		if(searchString.rfind("\n") != std::string::npos){
-			lastLine = searchString.substr(searchString.rfind("\n")+1);
-		}else{
+		if (searchString.rfind("\n") != std::string::npos) {
+			lastLine = searchString.substr(searchString.rfind("\n") + 1);
+		} else {
 			lastLine = searchString;
 		}
-		if(lastLine.find(" " LOGINSHELL " ") != std::string::npos){
+		if (lastLine.find(" " LOGINSHELL " ") != std::string::npos) {
 			return false;
 			break;
-		}else if(lastLine.find(COMMANDSHELL) != std::string::npos){
+		} else if (lastLine.find(COMMANDSHELL) != std::string::npos) {
 			return true;
 		}
 	}
@@ -65,7 +66,7 @@ bool ShellParser::isLoggedin(void){
 	return false;
 }
 
-bool ShellParser::login(const char* username, const char * password){
+bool ShellParser::login(const char* username, const char * password) {
 	LIBVMI_DEBUG_MSG("Login");
 	std::string string;
 	this->parseCommandOutput("", string);
@@ -74,23 +75,27 @@ bool ShellParser::login(const char* username, const char * password){
 	this->parseCommandOutput(username, string);
 	string.clear();
 	this->sendCommand(password);
-	usleep(1);
-	this->parseOutput(string);
-	std::cout << string << std::endl;
-	if(string.find("Login incorrect") != std::string::npos){
-		this->monitorShell = LOGINSHELL;
-		return false;
-	}else if(string.find(COMMANDSHELL) != std::string::npos){
-		this->monitorShell = COMMANDSHELL;
-		return true;
-	}else{
-		this->monitorShell = LOGINSHELL;
-		return false;
+	std::string buffer;
+	for (int searchCount = 3000; searchCount > 0; searchCount--) {
+		usleep(1000);
+		parseOutput(buffer);
+		string.append(buffer);
+		if (string.find("Login incorrect") != std::string::npos) {
+			this->monitorShell = LOGINSHELL;
+			return false;
+		} else if (string.find(COMMANDSHELL) != std::string::npos) {
+			this->monitorShell = COMMANDSHELL;
+			return true;
+		}
 	}
+
+	this->monitorShell = LOGINSHELL;
+	throw ConsoleMonitorException();
+	return false;
 }
 
-void ShellParser::logout(){
-	if(this->loggedin){
+void ShellParser::logout() {
+	if (this->loggedin) {
 		this->sendCommand("logout");
 		this->loggedin = false;
 	}
