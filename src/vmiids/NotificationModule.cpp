e07f279084dfccd00f65b749562681e26b51b693
Thomas Kittel <kittel@in.tum.de>
2010-08-11 17:19:13 +0200

    Default debug level is INFO

--- src/vmiids/NotificationModule.cpp
+++ src/vmiids/NotificationModule.cpp
@@ -13,20 +13,24 @@ namespace vmi {
 
 NotificationModule::NotificationModule(std::string moduleName): Module(moduleName) {
 	std::string debugLevelString;
-	GETOPTION(debugLevel, debugLevelString);
-	if (debugLevelString.compare("INFO") == 0) {
-		this->debugLevel = vmi::OUTPUT_INFO;
-	} else if (debugLevelString.compare("DEBUG") == 0) {
-		this->debugLevel = vmi::OUTPUT_DEBUG;
-	} else if (debugLevelString.compare("WARN") == 0) {
-		this->debugLevel = vmi::OUTPUT_WARN;
-	} else if (debugLevelString.compare("ERROR") == 0) {
-		this->debugLevel = vmi::OUTPUT_ERROR;
-	} else if (debugLevelString.compare("CRITICAL") == 0) {
-		this->debugLevel = vmi::OUTPUT_CRITICAL;
-	} else if (debugLevelString.compare("ALERT") == 0) {
-		this->debugLevel = vmi::OUTPUT_ALERT;
-	} else {
+	try{
+		GETOPTION(debugLevel, debugLevelString);
+		if (debugLevelString.compare("INFO") == 0) {
+			this->debugLevel = vmi::OUTPUT_INFO;
+		} else if (debugLevelString.compare("DEBUG") == 0) {
+			this->debugLevel = vmi::OUTPUT_DEBUG;
+		} else if (debugLevelString.compare("WARN") == 0) {
+			this->debugLevel = vmi::OUTPUT_WARN;
+		} else if (debugLevelString.compare("ERROR") == 0) {
+			this->debugLevel = vmi::OUTPUT_ERROR;
+		} else if (debugLevelString.compare("CRITICAL") == 0) {
+			this->debugLevel = vmi::OUTPUT_CRITICAL;
+		} else if (debugLevelString.compare("ALERT") == 0) {
+			this->debugLevel = vmi::OUTPUT_ALERT;
+		} else {
+			this->debugLevel = vmi::OUTPUT_INFO;
+		}
+	}catch(OptionNotFoundException &e){
 		this->debugLevel = vmi::OUTPUT_INFO;
 	}
 };
